1ce2c9a8c5531bbe18ce15cdd505efacd17113c1
bisterc <bistercas@gmail.com>
2025-02-01 18:28:51 -0500

    Rama Andres Castro

--- src/pages/events/form.tsx
+++ src/pages/events/form.tsx
@@ -1,23 +1,87 @@
-// import useAppStore from '@store/index';
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-//   const { role } = useAppStore();
+  const {
+    register,
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log("Formulario enviado", data);
+  };
 
   return (
-    <div className="cd-flex cd-items-center cd-flex-col cd-gap-y-[4rem]">
-     
-
-      <div className="cd-flex cd-flex-col cd-text-center cd-gap-y-[1rem]">
-        {/* {role === 'admin' && (
-          <a
-            className="cd-bg-violet-500 hover:cd-bg-violet-700 cd-text-white cd-font-bold cd-py-2 cd-px-4 cd-rounded"
-            href="/users/form"
-          >
-            Crear usuario
-          </a>
-        )} */}
-        FORM
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded-xl">
+      <h2 className="text-xl font-semibold text-center mb-4">Crear Evento</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <div>
+      <label className="block font-medium">Nombre</label>
+      <input
+       {...register("name", { required: "El nombre es obligatorio" })}
+        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:border-violet-500 focus:outline-none"
+        type="text"
+        placeholder="Tu nombre"
+      />
       </div>
+
+
+        <div>
+          <label className="block font-medium">Descripción</label>
+          <textarea
+            {...register("description", { required: "La descripción es obligatoria" })}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:border-violet-500 focus:outline-none"
+            placeholder="Descripción del evento"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium">Fecha</label>
+          <input
+            {...register("date", { required: "La fecha es obligatoria" })}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:border-violet-500 focus:outline-none"
+            type="date"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium">Monto</label>
+          <input
+            {...register("amount", { required: "El monto es obligatorio" })}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:border-violet-500 focus:outline-none"
+            type="number"
+            placeholder="0.00"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium">Tipo</label>
+          <select
+            {...register("type", { required: "El tipo es obligatorio" })}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:border-violet-500 focus:outline-none"
+          >
+            <option value="">Selecciona una opción</option>
+            <option value="income">Ingreso</option>
+            <option value="expense">Gasto</option>
+          </select>
+        </div>
+
+        <div>
+          <label className="block font-medium">Adjunto</label>
+          <div className="relative">
+          <input
+            {...register("attachment")}
+            className="w-full py-3 px-4 border border-gray-300 rounded-md text-sm text-gray-500 placeholder-gray-400 focus:ring-2 focus:ring-violet-500 focus:outline-none cursor-pointer"
+            type="file"
+          />
+        </div>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-violet-600 text-white py-2 rounded-md hover:bg-violet-700"
+        >
+          Enviar
+        </button>
+      </form>
     </div>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,28 @@
-const Home = () => {
 
+const Home = () => {
+  
   return (
+
+    
+    
     <div className="flex items-center flex-col gap-y-[4rem]">
      
       <h1 className="text-4xl font-bold text-center">Bienvenido!</h1>
 
       <div className="flex flex-col text-center gap-y-[1rem]">
       <a
-          className="cd-bg-blue-500 hover:cd-bg-blue-700 cd-text-white cd-font-bold cd-py-2 cd-px-4 cd-rounded"
+          className="w-full bg-violet-600 py-3 px-4 border border-gray-300 rounded-md text-sm text-white placeholder-gray-400 focus:ring-2 focus:ring-violet-500 focus:outline-none cursor-pointer"
           href="/form"
         >
           Ir a Form
+        
         </a>
+        
+        
       </div>
+      
     </div>
+    
   );
 };
 
